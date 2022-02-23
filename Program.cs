c04a537b9f6b6c3740ac905005f8b0be79cb7a39
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 23:19:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpWage_OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmpWage by OOPS Program");
+            
+        }
+    }
+}
