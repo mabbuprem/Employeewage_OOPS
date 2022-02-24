9e0b5a966092faada9639870e831c2fdeba7238b
mabbuprem <mabbupremkumar@gmail.com>
2022-02-24 22:27:50 +0530

    employee wage switch case

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -10,36 +10,35 @@ namespace EmpWage_OOPS
     {
         public static int EmployeeWage()
         {
-            //Constants
-
-            const int IS_PART_TIME = 1;
-            const int IS_FULL_TIME = 2;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int IS_ABSENT = 0;
-            const int EMP_RATE_PER_HOUR = 20;
-            int workingHours = 8;
-
-            //Variables
-            int empHrs = 0;
-
+            Console.WriteLine($"Employee Wage Calculator");
+            int workingHr = 1;
+            const int perHrSalary = 20;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            int present = (random.Next(0, 3));
+
+            switch (present)
             {
-                Console.WriteLine("Employee is doing parttime.");
-                empHrs = 4;
+
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is parttime");
+                    workingHr = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is fulltime");
+                    workingHr = 8;
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    workingHr = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is doing fulltime");
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent to the work");
-                empHrs = 0;
-            }
-            int empWage = workingHours * EMP_RATE_PER_HOUR;
+
+            int empWage = perHrSalary * workingHr;
             return empWage;
         }
     }
