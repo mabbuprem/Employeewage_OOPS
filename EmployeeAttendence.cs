4afd7077de0230c9dbc86e2ff3e8eb98c322ea56
mabbuprem <mabbupremkumar@gmail.com>
2022-02-24 22:32:58 +0530

    wage for month

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -13,32 +13,31 @@ namespace EmpWage_OOPS
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int IS_ABSENT = 0;
-            Console.WriteLine($"Employee Wage Calculator");
-            int workingHr = 1;
-            const int perHrSalary = 20;
+            const int WAGEPERHOUR = 20;
+
+            int workingHours = 0;
 
             Random random = new Random();
-            int present = (random.Next(0, 3));
-
-            switch (present)
+            int employeeCheck = random.Next(0, 3);
+            switch (employeeCheck)
             {
 
                 case IS_PART_TIME:
-                    Console.WriteLine("Employee is parttime");
-                    workingHr = 4;
+                    Console.WriteLine("Employee is part-time working");
+                    workingHours = 8;
                     break;
-
                 case IS_FULL_TIME:
-                    Console.WriteLine("Employee is fulltime");
-                    workingHr = 8;
+                    Console.WriteLine("Employee is present");
+                    workingHours = 8;
                     break;
                 default:
                     Console.WriteLine("Employee is absent");
-                    workingHr = 0;
+                    workingHours = 0;
                     break;
             }
 
-            int empWage = perHrSalary * workingHr;
+
+            int empWage = workingHours * WAGEPERHOUR;
             return empWage;
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,18 @@ namespace EmpWage_OOPS
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Calculation problem solving with OOPS concept");
-            int empWage = EmployeeAttendence.EmployeeWage();
-            Console.WriteLine(empWage);
+            int day = 0;
+            int empTotalWage = 0;
+            int MAXDAYS = 20;
+
+            while (day < MAXDAYS)
+            {
+                int empWage = EmployeeAttendence.EmployeeWage();
+                empTotalWage = empTotalWage + empWage;
+                Console.WriteLine($"Employee day {day + 1} wage is {empWage}");
+                day++;
+            }
+            Console.WriteLine("\nEmployee total wage is " + empTotalWage);
 
         }
     }
