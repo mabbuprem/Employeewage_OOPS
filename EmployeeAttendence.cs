9d61a006ec67d07d204576bcbd64b94133acd4c0
mabbuprem <mabbupremkumar@gmail.com>
2022-02-24 23:03:54 +0530

    total wage of company

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -4,46 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageComputation
+namespace EmpWage_OOPS
 {
-    class CalculateWorkingHrs
-    {
-        int workingHrs = 0;
-
-        public int GetWorkingHrs()
-        {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            //Constants
-            const int ABSENT = 0;
-            const int FULLTIME = 1;
-
-            switch (empCheck)
-            {
-                case ABSENT:
-                    workingHrs = 0;
-                    break;
-
-                case FULLTIME:
-                    workingHrs = 8;
-                    break;
-
-                default:
-                    workingHrs = 4;
-                    break;
-            }
-
-            return workingHrs;
-        }
-    }
-
-    class EmployeeWage
+    internal class EmployeeWage
     {
         public string companyName;
         public int maxDay;
         public int maxHrs;
         public int wagePerHr;
+        int empTotalWage = 0;
 
         public EmployeeWage(string companyName, int maxDay, int maxHrs, int wagePerHr)
         {
@@ -54,34 +23,54 @@ namespace EmployeeWageComputation
         }
 
 
-
         public void GetEmpWage()
         {
+            int workingHrs = 0;
             int day = 0;
             int totalWorkingHrs = 0;
-            int empTotalWage = 0;
 
-            CalculateWorkingHrs calculateWorkingHrs = new CalculateWorkingHrs();
 
             while (day < maxDay && totalWorkingHrs <= maxHrs)
             {
-                int workingHrs = calculateWorkingHrs.GetWorkingHrs();
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                //Constants
+                const int ABSENT = 0;
+                const int FULLTIME = 1;
+
+                switch (empCheck)
+                {
+                    case ABSENT:
+                        workingHrs = 0;
+                        break;
+
+                    case FULLTIME:
+                        workingHrs = 8;
+                        break;
+
+                    default:
+                        workingHrs = 4;
+                        break;
+                }
 
                 totalWorkingHrs = totalWorkingHrs + workingHrs;
 
                 int empWage = wagePerHr * workingHrs;
                 empTotalWage = empTotalWage + empWage;
 
-                Console.WriteLine($"Employee day {day + 1} wage is {empWage}");
+                //Console.WriteLine($"Employee day {day + 1} wage is {empWage}");
                 day++;
             }
-            Console.WriteLine($"\n {companyName} employee details");
-            Console.WriteLine("Total working hours is " + totalWorkingHrs);
-            Console.WriteLine("Total employee wage is " + empTotalWage);
 
+        }
+
+        public string ToString()
+        {
+            return "\nTotal employee wage for company " + companyName + " is " + empTotalWage;
 
         }
 
     }
-
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-namespace EmployeeWageComputation
+namespace EmpWage_OOPS
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Calculation problem solving with OOPS concept");
+            EmployeeWage dmart = new EmployeeWage("Dmart", 20, 100, 20);
+            EmployeeWage tata = new EmployeeWage("Tata", 25, 125, 30);
+            dmart.GetEmpWage();
+            Console.WriteLine(dmart.ToString());
+            tata.GetEmpWage();
+            Console.WriteLine(tata.ToString());
 
-            EmployeeWage employeeWage1 = new EmployeeWage("Dmart", 20, 100, 20);
-            EmployeeWage employeeWage2 = new EmployeeWage("Tata", 25, 125, 30);
-            employeeWage1.GetEmpWage();
-            employeeWage2.GetEmpWage();
         }
     }
 }
