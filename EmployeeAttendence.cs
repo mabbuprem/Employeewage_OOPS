63463aa371cd1f5d5ae02037fb74dc61aae8a782
mabbuprem <mabbupremkumar@gmail.com>
2022-02-24 22:23:25 +0530

    employee part time

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -8,17 +8,39 @@ namespace EmpWage_OOPS
 {
     internal class EmployeeAttendence
     {
-
-
-        public static void EmployeeAttendencee()
+        public static int EmployeeWage()
         {
-            const int fullTime = 1;
+            //Constants
+
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int IS_ABSENT = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            int workingHours = 8;
+
+            //Variables
+            int empHrs = 0;
+
+
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
-            if (empcheck == fullTime)
-                Console.WriteLine("Employee is present");
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is doing parttime.");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is doing fulltime");
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee is absent");
+            {
+                Console.WriteLine("Employee is absent to the work");
+                empHrs = 0;
+            }
+            int empWage = workingHours * EMP_RATE_PER_HOUR;
+            return empWage;
         }
     }
 
